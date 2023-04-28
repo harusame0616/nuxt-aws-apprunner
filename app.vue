220f122e7831ae126fc3e43fb604bbb2ab1b6ef9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-04-28 22:11:47 +0900

    Todoアプリ作成

--- app.vue
+++ app.vue
@@ -1,5 +1,59 @@
+<script setup lang="ts">
+const { todos, addTodo, removeTodo, toggleComplete } = useTodo();
+const title = ref("");
+
+const onSubmit = () => {
+  if (!title.value) {
+    return;
+  }
+
+  addTodo(title.value);
+  title.value = "";
+};
+
+const onCheckboxChange = (id: string) => {
+  toggleComplete(id);
+};
+
+const onDeleteClick = (id: string) => {
+  removeTodo(id);
+};
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>TODO SAMPLE</h1>
+    <main>
+      <form @submit.prevent="onSubmit">
+        <label for="title">タイトル</label>
+        <input type="text" v-model="title" id="title" />
+        <button>add</button>
+      </form>
+      <section>
+        <ul>
+          <li
+            v-for="todo of todos"
+            :key="todo.id"
+            :class="{ completed: !!todo.completeDate }"
+          >
+            <input
+              type="checkbox"
+              :value="!!todo.completeDate"
+              @change="onCheckboxChange(todo.id)"
+              :id="todo.id"
+            /><label :for="todo.id">{{ todo.title }}</label>
+            <button type="button" @click="onDeleteClick(todo.id)">
+              削除する
+            </button>
+          </li>
+        </ul>
+      </section>
+    </main>
   </div>
 </template>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
